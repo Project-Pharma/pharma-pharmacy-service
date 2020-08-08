65c3e8b0d7dddb7f5d8cfea0270264aeaa3bfb0b
Altay Batuhan <45828884+altarrok@users.noreply.github.com>
2020-08-08 15:18:00 -0700

    Initial Add

---
+++ src/main/java/com/inoastrum/pharmapharmacyservice/PharmaPharmacyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inoastrum.pharmapharmacyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaPharmacyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmaPharmacyServiceApplication.class, args);
+    }
+
+}
